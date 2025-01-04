1291a318265145317515351858bd0b17164e05e6
muttu <mutturajvittal998@gmail.com>
2025-01-04 19:02:52 +0530

    First commit

---
+++ src/java_basics/Helloworld.java
@@ -0,0 +1,10 @@
+package java_basics;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Good morning");
+        System.out.println("How are you?");
+        System.out.println("I'm Good");
+        System.out.print(".....");
+    }
+}
