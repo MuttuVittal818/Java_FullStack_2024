3613c7411aa0a018e85ae1b44d153e89fc2f38ba
muttu <mutturajvittal998@gmail.com>
2025-01-06 23:47:22 +0530

    changes done

--- src/java_basics/Helloworld.java
+++ src/java_basics/Helloworld.java
@@ -6,7 +6,5 @@ public class Helloworld {
         System.out.println("How are you?");
         System.out.println("I'm Good");
         System.out.print(".....");
-        System.out.println("123456789");
-        System.out.print("Thank you");
     }
 }
