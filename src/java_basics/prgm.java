ab0c626989af6cf37307643ff8690fa9a702f8f5
muttu <mutturajvittal998@gmail.com>
2025-01-10 22:09:57 +0530

    code updated

---
+++ src/java_basics/prgm.java
@@ -0,0 +1,10 @@
+package java_basics;
+
+public class prgm {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
