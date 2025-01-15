621831498632e7e344ea41cf8cb5c99913f5b508
muttu <mutturajvittal998@gmail.com>
2025-01-15 23:19:18 +0530

    Factorial of a number

---
+++ src/java_basics/Factorialofanumber.java
@@ -0,0 +1,16 @@
+package java_basics;
+
+public class Factorialofanumber {
+    public static void main(String[] args) {
+
+        int num = 9;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+
+}
