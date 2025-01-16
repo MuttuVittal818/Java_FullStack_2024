92be38ca7a9f424e67831e388600edc2f20beb03
muttu <mutturajvittal998@gmail.com>
2025-01-16 22:19:34 +0530

    Print Pattern

--- src/java_basics/Printpattern.java
+++ src/java_basics/Printpattern.java
@@ -3,9 +3,9 @@ package java_basics;
 public class Printpattern {
     public static void main(String[] args) {
 
-        int rows = 5;
+        int rows = 7;
 
-        for (int i = 1; i <= rows; ++i) {
+        for (int i = 0; i <= rows; ++i) {
 
             for (int j = 1; j <= i; ++j) {
 
