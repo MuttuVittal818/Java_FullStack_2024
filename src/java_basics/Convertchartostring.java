efc466f8b2964243fcb304ae4da4ad2af2921508
muttu <mutturajvittal998@gmail.com>
2025-01-17 19:38:22 +0530

    Convert char to string

--- src/java_basics/Convertchartostring.java
+++ src/java_basics/Convertchartostring.java
@@ -2,7 +2,7 @@ package java_basics;
 
 public class Convertchartostring {
     public static void main(String[] args) {
-        char[] ch = {'a', 'e', 'i', 'o', 'u'};
+        char[] ch = {'H', 'o', 'n', 'e', 'y'};
 
         String st = String.valueOf(ch);
         String st2 = new String(ch);
