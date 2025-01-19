3560e3ad27fc10000e55b344b17e1ccf37a3dca5
muttu <mutturajvittal998@gmail.com>
2025-01-19 22:29:17 +0530

    Add the elements of an Array

--- src/java_basics_programs/Addtheelements.java
+++ src/java_basics_programs/Addtheelements.java
@@ -3,7 +3,7 @@ package java_basics_programs;
 public class Addtheelements {
     public static void main(String args[]) {
 
-        int arr[] = {12, 13, 1, 10, 34, 10};
+        int arr[] = {9, 8, 15, 7, 3, -3};
         int sum = 0;
         for(int i=0; i<arr.length; i++)
         {
