fd10a90147972b18211a2756c31e54637fff36ea
muttu <mutturajvittal998@gmail.com>
2025-01-20 22:42:17 +0530

    find maximum of three numbers in -ve values

--- src/java_basics_programs/Findmaximumnumbers.java
+++ src/java_basics_programs/Findmaximumnumbers.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Findmaximumnumbers {
     public static void main(String args[]) {
-        int num1 = 15;
+        int num1 = -2;
         int num2 = -5;
-        int num3 = 7;
+        int num3 = 0;
         if (num1 >= num2 && num1 >= num3)
             System.out.println( num1 + " is the maximum number.");
         else if (num2 >= num1 && num2 >= num3)
