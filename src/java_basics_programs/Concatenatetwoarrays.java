0fa2b9fc06f7e33a8bb2171163e9804ec8841839
muttu <mutturajvittal998@gmail.com>
2025-01-21 22:59:57 +0530

    Concatenate Two Arrays using arraycopy

--- src/java_basics_programs/Concatenatetwoarrays.java
+++ src/java_basics_programs/Concatenatetwoarrays.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Concatenatetwoarrays {
     public static void main(String[] args) {
-        int[] array1 = {1, 2, 3};
-        int[] array2 = {4, 5, 6};
+        int[] array1 = {5, -2, 1};
+        int[] array2 = {-8, 3, 6};
 
         int[] result = new int[array1.length + array2.length];
 
