d5883fdc902b590820b9b70ac8a4db7340a455e8
muttu <mutturajvittal998@gmail.com>
2025-01-21 22:41:08 +0530

    Multi dimensional array

---
+++ src/java_basics_programs/Multidimensionalarray.java
@@ -0,0 +1,10 @@
+package java_basics_programs;
+
+import java.util.Arrays;
+
+public class Multidimensionalarray {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2}, {3, 4}, {5, 6, 7}};
+        System.out.println(Arrays.deepToString(array));
+    }
+}
