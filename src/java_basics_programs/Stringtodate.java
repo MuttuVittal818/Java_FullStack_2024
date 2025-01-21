8ab8c8545a31aecdb9cdca21c7863a4c17fac326
muttu <mutturajvittal998@gmail.com>
2025-01-21 22:46:15 +0530

    Convert String to Date using predefined formatters

--- src/java_basics_programs/Stringtodate.java
+++ src/java_basics_programs/Stringtodate.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class Stringtodate {
     public static void main(String[] args) {
         // Format y-M-d or yyyy-MM-d
-        String string = "2017-07-25";
+        String string = "2025-01-21";
         LocalDate date = LocalDate.parse(string, DateTimeFormatter.ISO_DATE);
 
         System.out.println(date);
