84a44b5184a544b26baff7ee92b453db5143366f
muttu <mutturajvittal998@gmail.com>
2025-01-22 23:10:51 +0530

    Find GCD of two numbers using for loop and if statement

--- src/java_basics_programs/GCDoftwonumbers.java
+++ src/java_basics_programs/GCDoftwonumbers.java
@@ -4,7 +4,7 @@ public class GCDoftwonumbers {
     public static void main(String[] args) {
 
         // find GCD between n1 and n2
-        int n1 = 81, n2 = 153;
+        int n1 = 92, n2 = 56;
 
         // initially set to gcd
         int gcd = 1;
