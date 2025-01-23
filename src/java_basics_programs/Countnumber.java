c616fecf104e2090c542c30a215d0f9bcd900d7a
muttu <mutturajvittal998@gmail.com>
2025-01-23 22:37:29 +0530

    Count Number of Digits in an Integer using for loop

--- src/java_basics_programs/Countnumber.java
+++ src/java_basics_programs/Countnumber.java
@@ -5,13 +5,15 @@ public class Countnumber {
 
         int count = 0, num = 01020050;
 
-        while (num != 0) {
+  /*      while (num != 0) {
             // num = num/10
             num /= 10;
             ++count;
+        }   */
+
+        for (; num != 0; num /= 10, ++count) {
         }
 
         System.out.println("Number of digits: " + count);
     }
-
 }
