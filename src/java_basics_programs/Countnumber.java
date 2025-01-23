3900041cf32293f262f2e5e349881d7c46c7b4b0
muttu <mutturajvittal998@gmail.com>
2025-01-23 22:34:32 +0530

    Count Number of Digits in an Integer using while loop

--- src/java_basics_programs/Countnumber.java
+++ src/java_basics_programs/Countnumber.java
@@ -3,7 +3,7 @@ package java_basics_programs;
 public class Countnumber {
     public static void main(String[] args) {
 
-        int count = 0, num = 2450030;
+        int count = 0, num = 01020050;
 
         while (num != 0) {
             // num = num/10
