cd494299f5ec7d0c19332748d04b51a382e5dda2
muttu <mutturajvittal998@gmail.com>
2025-01-23 23:34:59 +0530

    Join Two Lists using addAll()

--- src/java_basics_programs/Jointwolists.java
+++ src/java_basics_programs/Jointwolists.java
@@ -20,6 +20,5 @@ public class Jointwolists {
         System.out.println("list1: " + list1);
         System.out.println("list2: " + list2);
         System.out.println("joined: " + joined);
-
     }
 }
