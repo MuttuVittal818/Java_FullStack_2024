2bc0c42254a95e9d03a1f6f7a9b75ba92790298c
muttu <mutturajvittal998@gmail.com>
2025-01-24 23:48:05 +0530

    Compare two strings

--- src/java_basics_programs/Comparetwostrings.java
+++ src/java_basics_programs/Comparetwostrings.java
@@ -4,7 +4,7 @@ public class Comparetwostrings {
     public static void main(String[] args) {
 
         String style = "Bold";
-        String style2 = "Bold";
+        String style2 = "Cold";
 
         if(style == style2)
             System.out.println("Equal");
