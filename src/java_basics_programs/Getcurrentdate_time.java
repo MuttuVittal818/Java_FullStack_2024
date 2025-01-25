e9f36752c13d074a0b9202f62417c4dc5fb97426
muttu <mutturajvittal998@gmail.com>
2025-01-25 23:50:12 +0530

    Get Current date and time in default format

--- src/java_basics_programs/Getcurrentdate_time.java
+++ src/java_basics_programs/Getcurrentdate_time.java
@@ -1,11 +1,15 @@
 package java_basics_programs;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Getcurrentdate_time {
     public static void main(String[] args) {
-        LocalDateTime current = LocalDateTime.now();
+      LocalDateTime current = LocalDateTime.now();
 
-        System.out.println("Current Date and Time is: " + current);
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+      String formatted = current.format(formatter);
+
+      System.out.println("Current Date and Time is: " + formatted);
     }
 }
