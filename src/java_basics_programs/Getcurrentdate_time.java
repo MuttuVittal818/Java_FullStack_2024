fbc03b7fb832773e8229905fd357f0176fdf94b8
muttu <mutturajvittal998@gmail.com>
2025-01-27 00:30:51 +0530

    Get Current Date time in localized style

--- src/java_basics_programs/Getcurrentdate_time.java
+++ src/java_basics_programs/Getcurrentdate_time.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
+// Display Current date and time
+
 public class Getcurrentdate_time {
     public static void main(String[] args) {
       LocalDateTime current = LocalDateTime.now();
