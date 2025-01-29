1385a96dd051852d96fb4e0ed6ac1ebd5b325b3e
muttu <mutturajvittal998@gmail.com>
2025-01-29 19:51:22 +0530

    Merge two lists using the Stream class

--- src/java_basics_programs/Mergetwolists.java
+++ src/java_basics_programs/Mergetwolists.java
@@ -1,28 +1,32 @@
 package java_basics_programs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Mergetwolists {
     public static void main(String[] args) {
 
         // create first list
-        List<Integer> prime = new ArrayList<>();
-        prime.add(2);
-        prime.add(3);
-        prime.add(5);
+        // asList() converts the array into the arraylist
+        List<Integer> prime = new ArrayList<>(Arrays.asList(2, 3, 5));
         System.out.println("First List: " + prime);
 
         // create second list
-        List<Integer> even = new ArrayList<>();
-        even.add(4);
-        even.add(6);
+        List<Integer> even = new ArrayList<>(Arrays.asList(4, 6));
         System.out.println("Second List: " + even);
 
-        // create merged list
-        List<Integer> numbers = new ArrayList<>();
-        numbers.addAll(prime);
-        numbers.addAll(even);
+        // convert both lists into stream
+        Stream<Integer> list1 = prime.stream();
+        Stream<Integer> list2 = even.stream();
+
+        // merge two streams
+        Stream<Integer> merged = Stream.concat(list1, list2);
+
+        // convert the merged stream into list
+        List<Integer> numbers = merged.collect(Collectors.toList());
 
         System.out.println("Merged List: " + numbers);
 
