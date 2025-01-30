06848b0ae310e4bce07d33fe58b1c1ac3fc2facc
muttu <mutturajvittal998@gmail.com>
2025-01-30 20:49:10 +0530

    Display uppercased alphabet using for loop

---
+++ src/java_basics_programs/Displayuppercasedalphabet.java
@@ -0,0 +1,11 @@
+package java_basics_programs;
+
+public class Displayuppercasedalphabet {
+    public static void main(String[] args) {
+
+        char c;
+
+        for(c = 'A'; c <= 'Z'; ++c)
+            System.out.print(c + " ");
+    }
+}
