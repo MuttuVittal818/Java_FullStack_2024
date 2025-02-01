451b3a6700cc0e21a07eb621b7301d23b37827d1
muttu <mutturajvittal998@gmail.com>
2025-02-01 23:09:32 +0530

    Convert Map to List

---
+++ src/java_basics_programs/Convertmaptolist.java
@@ -0,0 +1,21 @@
+package java_basics_programs;
+
+import java.util.*;
+
+public class Convertmaptolist {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(3, "c");
+        map.put(4, "d");
+        map.put(5, "e");
+
+        List<Integer> keyList = new ArrayList(map.keySet());
+        List<String> valueList = new ArrayList(map.values());
+
+        System.out.println("Key List: " + keyList);
+        System.out.println("Value List: " + valueList);
+    }
+}
