a84c685412cb517672fbb9d6ce167a1f573df168
muttu <mutturajvittal998@gmail.com>
2025-02-01 23:26:22 +0530

    Pass ArrayList as Function Parameter by converting into Array

--- src/java_basics_programs/Passarraylist.java
+++ src/java_basics_programs/Passarraylist.java
@@ -10,17 +10,21 @@ public class Passarraylist {
             obtainedMarks += mark;
         }
 
+        // compute average
         double percent = (obtainedMarks * 100) / totalMarks;
         System.out.println("Percentage: " + percent);
     }
     public static void main(String[] args) {
 
+        // create an arraylist
         ArrayList<Integer> marks = new ArrayList<>();
         marks.add(67);
         marks.add(87);
         marks.add(56);
         System.out.println("Marks: " + marks);
 
+        // passing arraylist as function parameter
         percentage(marks.toArray(new Integer[marks.size()]));
     }
+
 }
