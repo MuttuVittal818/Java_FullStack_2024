a49be7a3fb4bc457fdd310050878b7e36ca60b82
muttu <mutturajvittal998@gmail.com>
2025-02-01 23:25:06 +0530

    Pass ArrayList as Function Parameter by converting into Array

--- src/java_basics_programs/Passarraylist.java
+++ src/java_basics_programs/Passarraylist.java
@@ -3,22 +3,24 @@ package java_basics_programs;
 import java.util.ArrayList;
 
 public class Passarraylist {
-    public static void display(ArrayList<String> languages) {
-
-        System.out.print("ArrayList: ");
-        for (String language : languages) {
-            System.out.print(language + ", ");
+    public static void percentage(Integer[] marks) {
+        int totalMarks = 300;
+        int obtainedMarks = 0;
+        for(int mark : marks) {
+            obtainedMarks += mark;
         }
+
+        double percent = (obtainedMarks * 100) / totalMarks;
+        System.out.println("Percentage: " + percent);
     }
     public static void main(String[] args) {
 
-        // create an arraylist
-        ArrayList<String> languages = new ArrayList<>();
-        languages.add("Java");
-        languages.add("Python");
-        languages.add("MySQL");
+        ArrayList<Integer> marks = new ArrayList<>();
+        marks.add(67);
+        marks.add(87);
+        marks.add(56);
+        System.out.println("Marks: " + marks);
 
-        // passing arraylist as function parameter
-        display(languages);
+        percentage(marks.toArray(new Integer[marks.size()]));
     }
 }
