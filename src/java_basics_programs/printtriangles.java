ff64eb6cc5aab703acadd9db6c3ab8e47f8209a0
muttu <mutturajvittal998@gmail.com>
2025-02-09 23:32:18 +0530

    Inverted half pyramid using *

--- src/java_basics_programs/printtriangles.java
+++ src/java_basics_programs/printtriangles.java
@@ -2,15 +2,14 @@ package java_basics_programs;
 
 public class printtriangles {
     public static void main(String[] args) {
-        char last = 'E', alphabet = 'A';
+        int rows = 5;
 
-        for (int i = 1; i <= (last - 'A' + 1); ++i) {
+        for (int i = rows; i >= 1; --i) {
             for (int j = 1; j <= i; ++j) {
-                System.out.print(alphabet + " ");
+                System.out.print("* ");
             }
-            ++alphabet;
-
             System.out.println();
+
         }
     }
 }
