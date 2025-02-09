fa2f6909c53afc1d382cc281e93549c521ebb5ef
muttu <mutturajvittal998@gmail.com>
2025-02-09 23:35:54 +0530

    Program to print pyramid using numbers

--- src/java_basics_programs/printtriangles.java
+++ src/java_basics_programs/printtriangles.java
@@ -2,20 +2,28 @@ package java_basics_programs;
 
 public class printtriangles {
     public static void main(String[] args) {
-        int rows = 5, k = 0;
+        int rows = 5, k = 0, count = 0, count1 = 0;
 
-        for (int i = 1; i <= rows; ++i, k = 0) {
+        for (int i = 1; i <= rows; ++i) {
             for (int space = 1; space <= rows - i; ++space) {
                 System.out.print("  ");
+                ++count;
             }
 
             while (k != 2 * i - 1) {
-                System.out.print("* ");
+                if (count <= rows - 1) {
+                    System.out.print((i + k) + " ");
+                    ++count;
+                } else {
+                    ++count1;
+                    System.out.print((i + k - 2 * count1) + " ");
+                }
+
                 ++k;
             }
+            count1 = count = k = 0;
 
             System.out.println();
-
         }
     }
 }
