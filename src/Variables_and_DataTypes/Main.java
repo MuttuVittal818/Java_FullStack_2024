f1f12fa6fef1454c6448cf82b7e7ae247a328538
muttu <mutturajvittal998@gmail.com>
2025-02-23 16:04:35 +0530

    Changes done

---
+++ src/Variables_and_DataTypes/Main.java
@@ -0,0 +1,20 @@
+package Variables_and_DataTypes;
+
+// Output Statements Print and Println
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+// Outputs text and adds a new line.
+
+        System.out.println("Good Morning");
+
+// Outputs text without a new line.
+
+        System.out.print("Hello");
+        System.out.print(" World");
+
+    }
+}
